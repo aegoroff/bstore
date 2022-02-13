47900c374895cbdde7afd4a85c640ce44c12109c
egoroff <egoroff@gmail.com>
2022-02-13 22:45:13 +0300

    Project initialization

---
+++ src/main.rs
@@ -0,0 +1,80 @@
+use env_logger::Env;
+use std::env;
+use std::net::SocketAddr;
+use std::path::Path;
+use warp::Filter;
+
+const DB_FILE: &str = "bstore.db";
+const CURRENT_DIR: &str = "./";
+
+extern crate tokio;
+#[macro_use]
+extern crate log;
+
+extern crate warp;
+
+#[tokio::main]
+async fn main() {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    let dir = env::var("BSTORE_DATA_DIR").unwrap_or_else(|_| String::from(CURRENT_DIR));
+    let db = Path::new(&dir).join(DB_FILE);
+    if !db.exists() {
+        // TODO: Create db
+        // Sqlite::open(db.clone(), Mode::ReadWrite)
+        //     .expect("Database file cannot be created")
+        //     .new_database()
+        //     .unwrap_or_default();
+    }
+
+    let port = env::var("RTDB_PORT").unwrap_or_else(|_| String::from("5000"));
+    let socket: SocketAddr = format!("0.0.0.0:{port}").parse().unwrap();
+
+    let routes = filters::routes(db)
+        .with(warp::cors().allow_any_origin())
+        .with(warp::log("bstore"));
+
+    warp::serve(routes).run(socket).await;
+}
+
+mod filters {
+    use super::*;
+
+    /// filters combined.
+    pub fn routes<P: AsRef<Path> + Clone + Send>(
+        db: P,
+    ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+        save(db.clone())
+    }
+
+    /// POST /api/save
+    fn save<P: AsRef<Path> + Clone + Send>(
+        db: P,
+    ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+        warp::path!("api" / "save")
+            .and(warp::post())
+            .and(with_db(db))
+            .and_then(handlers::save)
+    }
+
+    fn with_db<P: AsRef<Path> + Clone + Send>(
+        db: P,
+    ) -> impl Filter<Extract = (P,), Error = std::convert::Infallible> + Clone {
+        warp::any().map(move || db.clone())
+    }
+}
+
+mod handlers {
+    use super::*;
+    use std::convert::Infallible;
+    use std::time::Instant;
+    use warp::http::header::HeaderValue;
+    use warp::http::StatusCode;
+    use warp::path::Tail;
+    use warp::reply::{Json, Response};
+
+    pub async fn save<P: AsRef<Path> + Clone + Send>(
+        db: P,
+    ) -> Result<impl warp::Reply, Infallible> {
+        Ok(StatusCode::OK)
+    }
+}
