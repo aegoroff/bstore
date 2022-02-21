5546721aea0f1e31d9136f90c17fde55ddf61d86
egoroff <egoroff@gmail.com>
2022-02-21 19:05:33 +0300

    Revert "feat: file reading using stream draft"
    This reverts commit 54ba9b8874a712361c2d14d4967a12922a66c7bb.

--- src/file_reply.rs
+++ src/file_reply.rs
@@ -1,29 +1,25 @@
-use std::io::Read;
-use tokio_util::io::ReaderStream;
 use warp::hyper::Body;
 
 pub struct FileReply {
-    data: Box<dyn Read>,
+    data: Vec<u8>,
     name: String,
 }
 
 impl FileReply {
-    pub fn new(data: Box<dyn Read>, name: String) -> Self {
+    pub fn new(data: Vec<u8>, name: String) -> Self {
         Self { data, name }
     }
 }
 
 impl warp::Reply for FileReply {
     fn into_response(self) -> warp::reply::Response {
-        let mut stream = ReaderStream::new(self.data);
-
         let response = warp::http::Response::builder()
             .header("content-type", "application/octet-stream")
             .header(
                 "content-disposition",
                 format!("attachment; filename=\"{}\"", self.name),
             )
-            .body(Body::wrap_stream(stream))
+            .body(Body::from(self.data))
             .unwrap_or_default();
 
         response
