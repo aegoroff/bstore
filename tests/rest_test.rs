74a0e6500a319ea9936450bb7e0b981aceac2781
egoroff <egoroff@gmail.com>
2022-06-25 15:52:46 +0300

    test: integration test draft

--- tests/rest_test.rs
+++ tests/rest_test.rs
@@ -1,8 +1,18 @@
 use reqwest::Client;
 use reqwest::IntoUrl;
+use std::env;
 
-#[test]
-fn insert_many() {
+#[tokio::test]
+async fn insert_many() {
     // Arrange
+    let port = env::var("BSTORE_PORT").unwrap_or_else(|_| String::from("5000"));
     let client = Client::new();
+    let uri = format!("http://localhost:{port}");
+    let result = client.post(uri).send().await;
+    match result {
+        Ok(_) => todo!(),
+        Err(e) => {
+            println!("{}", e);
+        }
+    }
 }
