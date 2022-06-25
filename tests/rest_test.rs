7f430ac8e4ec17f79fd90dc1aa766572eece611d
egoroff <egoroff@gmail.com>
2022-06-25 21:18:11 +0300

    test: file test improved. Len detected automatically

--- tests/rest_test.rs
+++ tests/rest_test.rs
@@ -12,10 +12,11 @@ async fn insert_many_from_form() {
 
 
     let f = File::open("D:\\profile").await.unwrap();
+    let meta = f.metadata().await.unwrap();
     let stream = ReaderStream::new(f);
     let stream = reqwest::Body::wrap_stream(stream);
 
-    let part = reqwest::multipart::Part::stream_with_length(stream, 44462).file_name("profile");
+    let part = reqwest::multipart::Part::stream_with_length(stream, meta.len()).file_name("profile");
     let form = reqwest::multipart::Form::new().part("file", part);
 
     // Act
