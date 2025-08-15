0d1914b4890ab82ad0b95b1f975e250f6be7841a
Alexander Egorov <egoroff@gmail.com>
2025-08-16 01:42:10 +0300

    fix: different tags for different arch in docker

--- docker.sh
+++ docker.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 tag="ghcr.io/aegoroff/bstore:master"
-DOCKER_BUILDKIT=1 docker build . -t $tag
-DOCKER_BUILDKIT=1 docker build . -f DockerfileArm64 -t $tag --platform=linux/arm64
-docker push $tag
+DOCKER_BUILDKIT=1 docker build . -t "${tag}-x64"
+docker push "${tag}-x64"
+DOCKER_BUILDKIT=1 docker build . -f DockerfileArm64 -t "${tag}-arm64" --platform=linux/arm64
+docker push "${tag}-arm64"
