8ebe5620dc54d6d21817c036c51b51ed6a2c52d5
Alexander Egorov <egoroff@gmail.com>
2025-08-16 01:16:30 +0300

    feat: arm64 image building

--- docker.sh
+++ docker.sh
@@ -2,4 +2,5 @@
 
 tag="ghcr.io/aegoroff/bstore:master"
 DOCKER_BUILDKIT=1 docker build . -t $tag
+DOCKER_BUILDKIT=1 docker build . -f DockerfileArm64 -t $tag --platform=linux/arm64
 docker push $tag
