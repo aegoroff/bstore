035d4e134cd5654ba67664a2069c6c3b0fd93a33
Alexander Egorov <egoroff@gmail.com>
2025-08-21 09:06:29 +0300

    build: single docker image build and push

--- docker.sh
+++ docker.sh
@@ -2,6 +2,6 @@
 
 tag="registry.egoroff.spb.ru/egoroff/bstore:master"
 DOCKER_BUILDKIT=1 docker build . -t "${tag}-x64"
-docker push "${tag}-x64"
 DOCKER_BUILDKIT=1 docker build . -f DockerfileArm64 -t "${tag}-arm64" --platform=linux/arm64
-docker push "${tag}-arm64"
+docker manifest create $tag --amend ${tag}-x64 --amend ${tag}-arm64
+docker push $tag
