d929f40ffc7f7c93e0599da2e7001dfcf6ab6f67
Alexander Egorov <egoroff@gmail.com>
2025-08-21 12:49:56 +0300

    fix: pushich multi arch images fixed

--- docker.sh
+++ docker.sh
@@ -4,4 +4,6 @@ full_tag="registry.egoroff.spb.ru/egoroff/bstore:${TAG}"
 DOCKER_BUILDKIT=1 docker build . -t "${full_tag}-x64"
 DOCKER_BUILDKIT=1 docker build . -f DockerfileArm64 -t "${full_tag}-arm64" --platform=linux/arm64
 docker manifest create $full_tag --amend ${full_tag}-x64 --amend ${full_tag}-arm64
-docker push $full_tag
+docker push "${full_tag}-x64"
+docker push "${full_tag}-arm64"
+docker manifest push $full_tag
