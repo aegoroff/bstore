2a21996c00f2f8c1159694ac1f69ebd8c4a63502
Alexander Egorov <egoroff@gmail.com>
2025-08-28 19:47:54 +0300

    build: purge after mainfest pushing

--- docker.sh
+++ docker.sh
@@ -6,4 +6,4 @@ DOCKER_BUILDKIT=1 docker build . -f DockerfileArm64 -t "${full_tag}-arm64" --pla
 docker manifest create $full_tag --amend ${full_tag}-x64 --amend ${full_tag}-arm64
 docker push "${full_tag}-x64"
 docker push "${full_tag}-arm64"
-docker manifest push $full_tag
+docker manifest push --purge $full_tag
